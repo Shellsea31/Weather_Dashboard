3914cb93e0feade59a655eeaeaf6c86ea18fa6e8
Shellsea <elainesbarrera@gmail.com>
2020-11-04 11:39:39 -0800

    UPDATE: target srch btn, input, e-listener

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+// targeted search button and input area
+let searchBtn = document.querySelector("button")
+let input = document.querySelector("input")
+
+// event when search button is clicked
+searchBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    // right now it just console.logs the users text search
+    console.log(input.value);
+})
