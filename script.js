f5ab32463fcecdf48edad57fc00f5c7f2d0dc018
Shellsea <elainesbarrera@gmail.com>
2020-11-04 12:41:10 -0800

    UPDATE: var for temp, humidity, windSpeed

--- script.js
+++ script.js
@@ -11,13 +11,22 @@ searchBtn.addEventListener("click", function (e) {
 
 //   request data from weather api corresponding to the city
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=14a2df7296c80f13200f62bb2dd1f835`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=14a2df7296c80f13200f62bb2dd1f835`
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+        console.log(data)
+        let temperature = data.main.temp;
+        console.log(temperature);
+        let humidity = data.main.humidity;
+        console.log(humidity);
+        let windSpeed = data.wind.speed;
+        console.log(windSpeed);
+
     });
+
+    
     
 });
