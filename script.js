603f7edbefe61b565ac712ba2dc3f06790f6b2d7
Shellsea <elainesbarrera@gmail.com>
2020-11-04 12:14:40 -0800

    UPDATE: fetch request with param of city

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // targeted search button and input area
-let searchBtn = document.querySelector("button")
-let input = document.querySelector("input")
+let searchBtn = document.querySelector("button");
+let input = document.querySelector("input");
 
 // event when search button is clicked
-searchBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    // right now it just console.logs the users text search
-    console.log(input.value);
-})
+searchBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+//   grab the text from the input area
+  let city = input.value;
+  console.log(city);
+
+//   request data from weather api corresponding to the city
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=14a2df7296c80f13200f62bb2dd1f835`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+    
+});
